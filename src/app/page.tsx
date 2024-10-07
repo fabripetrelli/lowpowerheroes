50598a7e09f6a9195b637ed2f821a5c8ccd76922
fabripetrelli <petrellifabriziopaolo@gmail.com>
2024-10-07 18:34:51 +0200

    config changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="bg-black">
       <Image
-        src="/assets/IMG_0902.png"
+        src="../assets/IMG_0902.png"
         alt="coming soon..."
         width={5184}
         height={3456}
